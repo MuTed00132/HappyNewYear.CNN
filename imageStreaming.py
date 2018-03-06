436def38f72bddd89bd9fa24f449fa7c8323c07b
dataisfunny <35654613+dataisfunny@users.noreply.github.com>
2018-03-06 19:19:23 +0800

    all

---
+++ imageStreaming.py
@@ -0,0 +1,92 @@
+
+# coding: utf-8
+
+# In[5]:
+
+
+#匯入所需函數
+import cv2, sys, json, time, numpy as np, tensorflow as tf
+from pygame import mixer
+cascPath = "/home/ray/anaconda3/pkgs/opencv3-3.2.0-np111py35_0/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+from train import load_model, Model
+
+#result接收人臉辨識辨識結果並查詢audioList播放我們透過mensTalk函數錄好的恭喜發財錄音檔
+#t0;t1;waitTime是控制播放間隔參數
+t0=0;t1=0;waitTime=5;result = 0
+audioList = ['其他人','孫爺爺','發哥','志玲姊姊','范爺','阿妹','阿武','蔡姊']
+
+#初始化我們開發的人臉"分類"AI
+model = Model()
+model.load()
+#初始化音檔撥放功能
+mixer.init()
+
+
+# In[13]:
+
+
+# 攝影機連線
+cap = cv2.VideoCapture(0)
+
+while(True):
+  # 如果攝影機是開啟，則讀取圖片並使用try catch包住避免跳出錯誤
+  cap.isOpened()
+  try :
+    # 參數ret的值為True或False，代表有沒有讀到圖片，參數frame，是當前截取一幀的圖片
+    ret, frame = cap.read()
+    # Opencv內建的人臉辨識函式使需要灰階圖片故用cvtColor將BGR轉GRAY
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  except :
+    print("NO image")
+  #opencv3內建人臉識別功能函數
+  faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    #flags = cv2.cv.CV_HAAR_SCALE_IMAGE  #remove in opencv3
+    flags = cv2.CASCADE_SCALE_IMAGE
+  )
+  #將辨識出來的人臉透過for迴圈取出
+  for (x, y, w, h) in faces:
+    #標出人臉並畫線
+    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)  
+    image = frame[y - 70: y + h+70, x-20: x + w+20]
+    
+    #因為音檔有八秒鐘故使用t0、t1、waitTime三個參數控制分類頻率，
+    t0 = int(time.time())
+    if image is None :
+        print("")
+    else :
+      try :
+           #顯示圖片，看看顏色大小人臉辨識功能是否正常
+           cv2.imshow('frame', image)   
+           #因Opencv預設格式是BGR必須轉成RGB
+           image = image[:,:,::-1]
+       
+      except :
+           print("")
+      else :
+           #圖片傳入我們開發的人臉"分類"AI並得到結果  ;OpenCV faceCascade的是人臉"辨識"AI，我們開發的是人臉"分類"AI
+           result = model.predict(image)
+
+      #如果辨識結果不為0即其他人，且大於waitTime間隔
+      if result != 0 and t0-t1 > waitTime : 
+           #撥放對應音檔
+           videoPath = "./audio/"+audioList[result]+".mp3"
+           print (audioList[result]+" "+"Happy new year")
+           mixer.music.load(videoPath)
+           mixer.music.play()
+           t1=int(time.time())
+
+  # 若按下 q 鍵則離開迴圈
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+
+# 釋放攝影機
+cap.release()
+
+# 關閉所有 OpenCV 視窗
+cv2.destroyAllWindows()
+
